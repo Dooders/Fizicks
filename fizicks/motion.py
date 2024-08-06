ac70ea4436f621f622dd94b309d60c9c278ef5fc
Chris Mangum <peril10@hotmail.com>
2024-08-05 19:24:51 -0700

    Refactor motion module and add collision handling
    - Update type hints and imports
    - Rename 'Space' to 'Area' for consistency
    - Add collision detection and resolution in Motion.update()
    - Minor code style improvements

--- fizicks/motion.py
+++ fizicks/motion.py
@@ -1,15 +1,17 @@
 from typing import TYPE_CHECKING, Any
 
+from fizicks.collision import Collision
+
 if TYPE_CHECKING:
-    from fizicks.data import Vector
-    from fizicks.main import Space
+    from fizicks.data import Area, Force
+    from fizicks.matter import Matter
 
 
 class FirstLaw:
     """An object in motion will remain in motion unless acted on by an external force."""
 
     @classmethod
-    def apply(cls, object: Any, force: "Vector"):
+    def apply(cls, object: "Matter", force: "Force") -> None:
         """
         Updates the velocity of the object based on the force applied.
         """
@@ -18,18 +20,18 @@ class FirstLaw:
 
 class SecondLaw:
     @classmethod
-    def apply(cls, object: Any, space: "Space"):
+    def apply(cls, object: "Matter", area: "Area") -> None:
         """
         Updates the position of the object based on the velocity.
         """
         object.position = object.position + object.velocity
-        if space.toroidal:
-            object.position = object.position % space.dimensions
+        if area.has_boundaries:
+            object.position = object.position % area.dimensions
 
 
 class ThirdLaw:
     @classmethod
-    def apply(cls, object: Any):
+    def apply(cls, object: "Matter") -> None:
         """
         Updates the acceleration of the object based on the velocity and mass.
         """
@@ -40,14 +42,15 @@ class Motion:
     """The motion of an object is determined by the sum of its forces and the net force acting on it."""
 
     @classmethod
-    def update(cls, object: Any, space: "Space") -> None:
+    def update(cls, object: Any, area: "Area") -> None:
         """
         Updates the object based on the forces applied and its current state.
         """
         if object.debt:
             for debt in object.debt:
                 FirstLaw.apply(object, debt)
-        SecondLaw.apply(object, space)
+        SecondLaw.apply(object, area)
         ThirdLaw.apply(object)
-
+        Collision.detect(object, area)
+        Collision.resolve(object, area)
         object.debt = []
