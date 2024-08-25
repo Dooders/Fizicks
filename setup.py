22fc4e7cbf0e3d40a7b3f952f3b4cbb9c9a48e22
Chris Mangum <csmangum@gmail.com>
2024-08-25 16:30:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fizicks",
-    version="0.2.1",
+    version="0.2.2",
     author="Chris Mangum",
     author_email="csmangum@gmail.com",
     description="A state-driven physics model designed to simulate cause and effect within a physical space.",
